3862d8b60404efa64fcaf8ce9e86476975629c64
Kersti Miller <kersti.miller@bigbank.ee>
2024-05-03 17:14:51 +0300

    Create form validation composable.

---
+++ src/composables/formValidation.ts
@@ -0,0 +1,39 @@
+import { computed, ref } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import { required, email, minValue, minLength, maxLength } from '@vuelidate/validators'
+import type { Form } from '@/types';
+
+export function useFormValidation(form: Form) {
+  const startWith55 = (value: string) => value.startsWith('55');
+
+  const rules = computed(() => {
+    return {
+      firstName: { required },
+      lastName: { required },
+      email: {
+        required,
+        email,
+      },
+      mobileNumber: {
+        required,
+        minLength: minLength(7),
+        maxLength: maxLength(8),
+        startWith55: {
+          $validator: startWith55,
+          $message: 'Mobile number must start with "55"',
+        },
+        
+      },
+      monthlyIncome: {
+        required,
+        minValue: minValue(100),
+      },
+    }
+  })
+
+  const v$ = useVuelidate(rules, form);
+
+  return {
+    v$,
+  };
+}
